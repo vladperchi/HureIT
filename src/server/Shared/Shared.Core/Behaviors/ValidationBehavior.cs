5031030b927d8a3813aef258d88563e56d82452f
vladperchi <vladperchi@gmail.com>
2022-05-25 10:27:41 -0500

    feat(DEV): Added Validation Errors

---
+++ src/server/Shared/Shared.Core/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ValidationBehavior.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HureIT.Shared.Core.Exceptions;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Shared.Core.Behaviors
+{
+    public class ValidationBehavior
+    {
+    }
+
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+         where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        private readonly IStringLocalizer<ValidationBehavior> _localizer;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators, IStringLocalizer<ValidationBehavior> localizer)
+        {
+            _validators = validators;
+            _localizer = localizer;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                {
+                    var errors = failures.Select(a => a.ErrorMessage).Distinct().ToList();
+                    throw new ValidationCustomException(_localizer, errors);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ src/server/Shared/Shared.Core/Exceptions/ValidationCustomException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ValidationCustomException.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Shared.Core.Exceptions
+{
+    public class ValidationCustomException : CustomException
+    {
+        public ValidationCustomException(IStringLocalizer localizer, List<string> errors)
+            : base(localizer["Validation errors have occurred..."], errors, HttpStatusCode.BadRequest)
+        {
+        }
+    }
+}
