328f733a38d8eca0c6dee5b6ef9e0a6c3e0c0913
vladperchi <vladperchi@gmail.com>
2022-05-25 17:41:27 -0500

    feat(DEV): Created Generic Types

---
+++ src/server/Shared/Shared.Core/Utilities/TypeUtilities.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="TypeUtilities.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+
+namespace HureIT.Shared.Core.Utilities
+{
+    public static class TypeUtilities
+    {
+        public static string GetGenericTypeName(this Type type)
+        {
+            if (type.IsGenericType)
+            {
+                string genericTypes = string.Join(",", type.GetGenericArguments().Select(GetGenericTypeName).ToArray());
+                return $"{type.Name.Remove(type.Name.IndexOf('`'))}<{genericTypes}>";
+            }
+
+            return type.Name;
+        }
+    }
+}
