0c22a4bd50147c504a9750fc4bd28deff573aefb
vladperchi <vladperchi@gmail.com>
2022-05-25 10:22:45 -0500

    feat(DEV): Added File Type

---
+++ src/server/Shared/Shared.Core/Common/Enums/FileType.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="FileType.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.ComponentModel;
+
+namespace HureIT.Shared.Core.Common.Enums
+{
+    public enum FileType
+    {
+        [Description(".jpg|.png|.jpeg")]
+        Image
+    }
+}
