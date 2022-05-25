d67d0fc164d108505c146eaf4ba345b3728a4cf0
vladperchi <vladperchi@gmail.com>
2022-05-24 23:26:58 -0500

    feat(DEV): Created DTOs Upload File Application

---
+++ src/server/Shared/Shared.DTO/Upload/UploadStorageType.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UploadStorageType.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.ComponentModel;
+
+namespace HureIT.Shared.DTO.Upload
+{
+    public enum UploadStorageType
+    {
+        [Description(@"Images\User\Staff")]
+        Staff,
+
+        [Description(@"Images\Person\Employees")]
+        Employee
+    }
+}
