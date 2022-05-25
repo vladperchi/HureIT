a7e2509b9acb125d889542ed504bd039d36fbf0b
vladperchi <vladperchi@gmail.com>
2022-05-25 17:42:29 -0500

    feat(DEV): Created Contracts Application

---
+++ src/server/Shared/Shared.Core/Contracts/IBaseEntity.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IBaseEntity.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using HureIT.Shared.Core.Domain;
+
+namespace HureIT.Shared.Core.Contracts
+{
+    public interface IBaseEntity
+    {
+        public IReadOnlyCollection<Event> DomainEvents { get; }
+
+        public void AddDomainEvent(Event domainEvent);
+
+        public void RemoveDomainEvent(Event domainEvent);
+
+        public void ClearDomainEvents();
+    }
+}
---
+++ src/server/Shared/Shared.Core/Contracts/IEntity.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IEntity.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Shared.Core.Contracts
+{
+    public interface IEntity<TEntityId> : IEntity
+    {
+        public TEntityId Id { get; set; }
+    }
+
+    public interface IEntity
+    {
+    }
+}
