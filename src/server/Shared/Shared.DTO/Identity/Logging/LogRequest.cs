c475f0fb359c67766df866bbbe098323089f2d6b
vladperchi <vladperchi@gmail.com>
2022-05-24 23:04:41 -0500

    feat(DEV): Created DTOs Logging Identity

---
+++ src/server/Shared/Shared.DTO/Identity/Logging/LogRequest.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="LogRequest.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace HureIT.Shared.DTO.Identity.Logging
+{
+    public class LogRequest
+    {
+        public LogRequest()
+        {
+        }
+
+        public string Event { get; set; }
+
+        public string Description { get; set; }
+
+        public string Email { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
---
+++ src/server/Shared/Shared.DTO/Identity/Logging/PaginatedLogFilter.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PaginatedLogFilter.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using HureIT.Shared.DTO.Filters;
+
+namespace HureIT.Shared.DTO.Identity.Logging
+{
+    public class PaginatedLogFilter : PaginatedFilter
+    {
+        public string SearchString { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string MessageType { get; set; }
+    }
+}
