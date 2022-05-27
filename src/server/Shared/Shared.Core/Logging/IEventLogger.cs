2ea9ecb68baa3e1b8eaa81824c0fc2f5e6025a68
vladperchi <vladperchi@gmail.com>
2022-05-26 20:15:00 -0500

    feat(DEV): Event Log Application

---
+++ src/server/Shared/Shared.Core/Logging/IEventLogger.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IEventLogger.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using HureIT.Shared.Core.Domain;
+
+namespace HureIT.Shared.Core.Logging
+{
+    public interface IEventLogger
+    {
+        Task SaveAsync<T>(T @event, (string oldValues, string newValues) changes)
+            where T : Event;
+    }
+}
