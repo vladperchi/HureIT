c656192daeb6524f1718e4d0d461b1b2446b230b
vladperchi <vladperchi@gmail.com>
2022-05-26 20:51:14 -0500

    feat(settings): Added Setting Mail

---
+++ src/server/Shared/Shared.Core/Settings/MailSettings.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="MailSettings.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Shared.Core.Settings
+{
+    public class MailSettings
+    {
+        public string From { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public bool EnableVerification { get; set; }
+    }
+}
