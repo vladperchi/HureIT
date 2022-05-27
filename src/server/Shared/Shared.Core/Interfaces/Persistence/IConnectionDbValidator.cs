2fc4efc35e3f4fa698b4e079a58e31e6dbcb2970
vladperchi <vladperchi@gmail.com>
2022-05-26 22:20:07 -0500

    feat(interfaces): Added Validator Connection

---
+++ src/server/Shared/Shared.Core/Interfaces/Persistence/IConnectionDbValidator.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IConnectionDbValidator.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace HureIT.Shared.Core.Interfaces.Persistence
+{
+    public interface IConnectionDbValidator
+    {
+        Task<bool> TryValidate(string connectionString = null, string dataProvider = null);
+    }
+}
