289ca7299e89f65215d819807d9f2796f1da3e4f
vladperchi <vladperchi@gmail.com>
2022-05-26 20:46:40 -0500

    feat(settings): Added Setting Application

---
+++ src/server/Shared/Shared.Core/Settings/ApplicationSettings.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationSettings.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Shared.Core.Settings
+{
+    public class ApplicationSettings
+    {
+        public string ApiUrl { get; set; }
+    }
+}
