1afe906fafcc8ef01310f8379f9ce1f7a2f73fae
vladperchi <vladperchi@gmail.com>
2022-05-26 22:25:25 -0500

    feat(interfaces): Added Paginated Filter Validator

---
+++ src/server/Shared/Shared.Core/Interfaces/Validators/IPaginatedFilterValidator.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IPaginatedFilterValidator.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using HureIT.Shared.Core.Contracts;
+using HureIT.Shared.Core.Extensions;
+using HureIT.Shared.DTO.Filters;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Shared.Core.Interfaces.Validators
+{
+    internal interface IPaginatedFilterValidator<TEntityId, TEntity, TFilter>
+        where TEntity : class, IEntity<TEntityId>
+        where TFilter : PaginatedFilter
+    {
+        static void UseRules(AbstractValidator<TFilter> validator, IStringLocalizer localizer)
+        {
+            validator.RuleFor(request => request.PageNumber)
+                .GreaterThan(0).WithMessage(localizer["{PropertyName} must be greater than 0."]);
+            validator.RuleFor(request => request.PageSize)
+                .GreaterThan(0).WithMessage(localizer["{PropertyName} must be greater than 0."]);
+            validator.RuleFor(request => request.OrderBy)
+                .MustContainCorrectOrderingsFor(typeof(TEntity), localizer);
+        }
+    }
+}
