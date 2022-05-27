e29e4e4d31f4846f3b03659e586ed3495ec7b390
vladperchi <vladperchi@gmail.com>
2022-05-26 20:52:28 -0500

    feat(settings): Added Setting Connection String

---
+++ src/server/Shared/Shared.Core/Settings/PersistenceSettings.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PersistenceSettings.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Shared.Core.Settings
+{
+    public class PersistenceSettings
+    {
+        public bool UseMsSql { get; set; }
+
+        public PersistenceConnectionStrings ConnectionStrings { get; set; }
+
+        public class PersistenceConnectionStrings
+        {
+            public string MSSQL { get; set; }
+        }
+    }
+}
