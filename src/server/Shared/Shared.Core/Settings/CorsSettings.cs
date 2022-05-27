670fc75167737407666eaf5c945280606d97c73d
vladperchi <vladperchi@gmail.com>
2022-05-26 20:49:20 -0500

    feat(settings): Added Setting Cors

---
+++ src/server/Shared/Shared.Core/Settings/CorsSettings.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="CorsSettings.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace ElevatorIT.Shared.Core.Settings
+{
+    public class CorsSettings
+    {
+        public string AngularUrl { get; set; }
+    }
+}
