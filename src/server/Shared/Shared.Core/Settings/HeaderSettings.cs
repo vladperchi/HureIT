0d4f9c0383704aa6d98c7c5faecbb3b8cd643a91
vladperchi <vladperchi@gmail.com>
2022-05-26 20:51:39 -0500

    feat(settings): Added Setting Header

---
+++ src/server/Shared/Shared.Core/Settings/HeaderSettings.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="HeaderSettings.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Shared.Core.Settings
+{
+    public class HeaderSettings
+    {
+        public bool Enable { get; set; }
+
+        public string XFrameOptions { get; set; }
+
+        public string XContentTypeOptions { get; set; }
+
+        public string ReferrerPolicy { get; set; }
+
+        public string PermissionsPolicy { get; set; }
+
+        public string SameSite { get; set; }
+
+        public string XXSSProtection { get; set; }
+
+        public string ContentPolicy { get; set; }
+    }
+}
