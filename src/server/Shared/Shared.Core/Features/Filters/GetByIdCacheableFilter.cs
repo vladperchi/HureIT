c54ce39ee7f67dc7a42b92eb6b69be92253831d0
vladperchi <vladperchi@gmail.com>
2022-05-26 22:31:15 -0500

    feat(DEV): Added Cacheable Filter

---
+++ src/server/Shared/Shared.Core/Features/Filters/GetByIdCacheableFilter.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GetByIdCacheableFilter.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using HureIT.Shared.Core.Constants;
+using HureIT.Shared.Core.Contracts;
+using HureIT.Shared.Core.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HureIT.Shared.Core.Features.Filters
+{
+    public class GetByIdCacheableFilter<TEntityId, TEntity> : ICacheable
+        where TEntity : class, IEntity<TEntityId>
+    {
+        [FromRoute(Name = "id")]
+        public TEntityId Id { get; set; }
+
+        [FromQuery]
+        public bool SkipCache { get; set; }
+
+        [FromQuery]
+        public string CacheKey => CacheKeys.Common.GetEntityByIdCacheKey<TEntityId, TEntity>(Id);
+
+        [FromQuery]
+        [RegularExpression(StringKeys.SchemaRegexPattern)]
+
+        public TimeSpan? Expiration { get; set; }
+    }
+}
