87a2343c31905475b46e7607d14584812b87b629
vladperchi <vladperchi@gmail.com>
2022-05-26 20:25:52 -0500

    feat(DEV): Custom Result Response

---
+++ src/server/Shared/Shared.Core/Wrapper/IResult.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IResult.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace HureIT.Shared.Core.Wrapper
+{
+    public interface IResult
+    {
+        List<string> Messages { get; set; }
+
+        bool Succeeded { get; set; }
+    }
+
+    public interface IResult<out T> : IResult
+    {
+        T Data { get; }
+    }
+}
