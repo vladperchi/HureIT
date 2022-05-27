6601bc5060f5a07e48627a4badfa308ee4f3ee1a
vladperchi <vladperchi@gmail.com>
2022-05-26 22:47:34 -0500

    feat(DEV): Added Queryable Extension

---
+++ src/server/Shared/Shared.Core/Extensions/QueryableExtensions.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="QueryableExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HureIT.Shared.Core.Exceptions;
+using HureIT.Shared.Core.Wrapper;
+using HureIT.Shared.Core.Contracts;
+using HureIT.Shared.Core.Interfaces.Specifications;
+
+namespace HureIT.Shared.Core.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize)
+            where T : class
+        {
+            if (source == null)
+            {
+                throw new PagedListEmptyException();
+            }
+
+            pageNumber = pageNumber == 0 ? 1 : pageNumber;
+            pageSize = pageSize == 0 ? 10 : pageSize;
+            int count = await source.AsNoTracking().CountAsync();
+            pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return PaginatedResult<T>.Success(items, count, pageNumber, pageSize);
+        }
+
+        public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> spec)
+            where T : class, IEntity
+        {
+            var queryableResultWithIncludes = spec.Includes
+                .Aggregate(
+                    query,
+                    (current, include) => current.Include(include));
+            var secondaryResult = spec.IncludeStrings
+                .Aggregate(
+                    queryableResultWithIncludes,
+                    (current, include) => current.Include(include));
+            return secondaryResult.Where(spec.Criteria);
+        }
+    }
+}
