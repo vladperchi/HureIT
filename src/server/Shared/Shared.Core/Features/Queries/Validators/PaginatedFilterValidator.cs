f1b6aa878a0d5f19c1e73290bafed1d5d4aa5d8a
vladperchi <vladperchi@gmail.com>
2022-05-26 22:32:00 -0500

    feat(DEV): Added Paginated Filter Validator

---
+++ src/server/Shared/Shared.Core/Features/Queries/Validators/PaginatedFilterValidator.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PaginatedFilterValidator.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using FluentValidation;
+using HureIT.Shared.Core.Contracts;
+using HureIT.Shared.Core.Interfaces.Validators;
+using HureIT.Shared.DTO.Filters;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Shared.Core.Features.Queries.Validators
+{
+    public abstract class PaginatedFilterValidator<TEntityId, TEntity, TFilter> :
+        AbstractValidator<TFilter>,
+        IPaginatedFilterValidator<TEntityId, TEntity, TFilter>
+            where TEntity : class, IEntity<TEntityId>
+            where TFilter : PaginatedFilter
+    {
+        protected PaginatedFilterValidator(IStringLocalizer localizer)
+        {
+            IPaginatedFilterValidator<TEntityId, TEntity, TFilter>.UseRules(this, localizer);
+        }
+    }
+}
