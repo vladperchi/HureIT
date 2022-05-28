405866e55cd303b5bc3a1d0bfb395b4aef8de705
vladperchi <vladperchi@gmail.com>
2022-05-28 11:59:30 -0500

    feat(DEV): Added Common Utilities

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/EnumExtensions.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EnumExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace HureIT.Shared.Infrastructure.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescriptionString(this Enum value)
+        {
+            var type = value.GetType();
+            if (!type.IsEnum) throw new ArgumentException($"Type '{type}' is not Enum");
+
+            var attributes = (DescriptionAttribute[])value
+                .GetType()
+                .GetField(value.ToString())?
+                .GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes is { Length: > 0 }
+                 ? attributes[0].Description
+                 : value.ToString();
+        }
+
+        public static List<string> GetDescriptionList(this Enum val)
+        {
+            string result = val.ToDescriptionString();
+            return result.Split('|').ToList();
+        }
+    }
+}
