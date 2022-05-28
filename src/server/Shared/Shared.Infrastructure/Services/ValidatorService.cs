b83a7a04d9dbf1d610f71fc1c1bcbc8c9e59115c
vladperchi <vladperchi@gmail.com>
2022-05-28 13:33:43 -0500

    feat(Service): Added Validation

---
+++ src/server/Shared/Shared.Infrastructure/Services/ValidatorService.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ValidatorService.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HureIT.Shared.Core.Interfaces.Services;
+
+namespace ElevatorIT.Shared.Infrastructure.Services
+{
+    public class ValidatorService : IValidatorService
+    {
+        public Task<bool> IsOnlyLetterAndSpace(string value) =>
+             Task.FromResult(Regex.IsMatch(value, @"^(?! )[A-Za-z\s]+$"));
+
+        public Task<bool> IsLetterOrDigit(string value) =>
+            Task.FromResult(value.All(char.IsLetterOrDigit));
+
+        public Task<bool> IsOnlyNumber(int value) =>
+            Task.FromResult(value.ToString().All(char.IsNumber));
+    }
+}
