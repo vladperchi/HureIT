834fd0ba9952cecb73c6a1342d9fbf5fcde08d53
vladperchi <vladperchi@gmail.com>
2022-05-28 11:28:51 -0500

    feat(DEV): Added Services

--- src/server/Api/Bootstrapper/Startup.cs
+++ src/server/Api/Bootstrapper/Startup.cs
@@ -6,47 +6,36 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------
 
+using HureIT.Shared.Core.Extensions;
+using HureIT.Shared.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace Bootstrapper
+namespace HureIT.Bootstrapper
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddDistributedMemoryCache()
+                .AddSerialization(_config)
+                .AddSharedInfrastructure(_config)
+                .AddSharedApplication(_config);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseSharedInfrastructure(_config);
         }
     }
 }
