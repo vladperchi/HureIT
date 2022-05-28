29c0cfab29d1f36442e9913feda4fb679af2c489
vladperchi <vladperchi@gmail.com>
2022-05-28 11:27:50 -0500

    feat(DEV): Configured Api Init

--- src/server/Api/Bootstrapper/Program.cs
+++ src/server/Api/Bootstrapper/Program.cs
@@ -6,23 +6,50 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------
 
+using System;
+using System.Threading;
+using HureIT.Shared.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
-namespace Bootstrapper
+namespace HureIT.Bootstrapper
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            try
+            {
+                Log.Information("Server Starting Up...");
+                Log.Information("Welcome {Name} from thread {ThreadId}", Environment.UserName, Thread.CurrentThread.ManagedThreadId);
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Something went wrong. Server failed...");
+            }
+            finally
+            {
+                Log.Information("Server Shutting down...");
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }).ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(LogLevel.Debug);
+
+                // logging.AddDebug();
+            });
     }
 }
