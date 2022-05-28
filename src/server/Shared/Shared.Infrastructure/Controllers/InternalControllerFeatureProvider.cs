c2b29b00cf3ec4e3ba1d63efdc3429408e1ecd0f
vladperchi <vladperchi@gmail.com>
2022-05-28 11:50:49 -0500

    feat(DEV): Added Controller Feature Provider

---
+++ src/server/Shared/Shared.Infrastructure/Controllers/InternalControllerFeatureProvider.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="InternalControllerFeatureProvider.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace HureIT.Shared.Infrastructure.Controllers
+{
+    /// <inheritdoc cref = "ControllerFeatureProvider" />
+    internal class InternalControllerFeatureProvider : ControllerFeatureProvider
+    {
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            if (!typeInfo.IsClass)
+            {
+                return false;
+            }
+
+            if (typeInfo.IsAbstract)
+            {
+                return false;
+            }
+
+            if (typeInfo.ContainsGenericParameters)
+            {
+                return false;
+            }
+
+            if (typeInfo.IsDefined(typeof(NonControllerAttribute)))
+            {
+                return false;
+            }
+
+            if (!typeInfo.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase) ||
+                !typeInfo.IsDefined(typeof(ControllerAttribute)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
