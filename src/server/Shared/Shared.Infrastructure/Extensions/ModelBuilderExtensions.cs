46dadf26dd17931c551165353ee24627075d148c
vladperchi <vladperchi@gmail.com>
2022-05-28 13:46:58 -0500

    feat(DEV): Added Shared Infrastructure Service

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ModelBuilderExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using HureIT.Shared.Core.Settings;
+using Microsoft.EntityFrameworkCore;
+
+namespace HureIT.Shared.Infrastructure.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyApplicationConfiguration(this ModelBuilder builder, PersistenceSettings persistenceOptions)
+        {
+            // Set column type (Float, Decimal, DateTime, etc)
+        }
+
+        public static void ApplyModuleConfiguration(this ModelBuilder builder, PersistenceSettings persistenceOptions)
+        {
+            // Build model for MSSQL
+        }
+    }
+}
---
+++ src/server/Shared/Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using HureIT.Shared.Core.Logging;
+using HureIT.Shared.Core.Exceptions;
+using HureIT.Shared.Core.Interfaces.Contexts;
+using HureIT.Shared.Core.Settings;
+using HureIT.Shared.Infrastructure.Controllers;
+using HureIT.Shared.Infrastructure.Logging;
+using HureIT.Shared.Infrastructure.Interceptors;
+using HureIT.Shared.Infrastructure.Persistence;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using HureIT.Shared.Infrastructure.Persistence.Connection;
+using HureIT.Shared.Core.Interfaces.Persistence;
+using HureIT.Shared.Core.Constants;
+
+[assembly: InternalsVisibleTo(StringKeys.AssemblyName)]
+
+namespace HureIT.Shared.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        internal static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddPersistenceSettings(config);
+            services.AddTransient<IConnectionDbSure, ConnectionDbSure>();
+            services.AddTransient<IConnectionDbValidator, ConnectionDbValidator>();
+            services
+                .AddDatabaseContext<ApplicationDbContext>()
+                .AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            services.AddScoped<IEventLogger, EventLogger>();
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            });
+            services.AddControllers()
+                .ConfigureApplicationPartManager(manager =>
+                {
+                    manager.FeatureProviders.Add(new InternalControllerFeatureProvider());
+                })
+                .AddMvcOptions(options =>
+                {
+                    options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((value, propertyName) =>
+                        throw new CustomException($"{propertyName}: value '{value}' is not valid.", statusCode: HttpStatusCode.BadRequest));
+                });
+            services.AddTransient<IValidatorInterceptor, ValidatorInterceptor>();
+            services.AddApplicationLayer(config);
+            services.AddHangfireJobs(config);
+            services.AddExceptionMiddleware();
+            services.AddSwaggerDocumentation();
+            services.AddCorsPolicy();
+            services.AddApplicationSettings(config);
+            services.AddLocalization(options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
+            services.AddRouting(options => options.LowercaseUrls = true);
+            return services;
+        }
+
+        private static IServiceCollection AddPersistenceSettings(this IServiceCollection services, IConfiguration config)
+        {
+            return services.Configure<PersistenceSettings>(config.GetSection(nameof(PersistenceSettings)));
+        }
+
+        private static IServiceCollection AddApplicationSettings(this IServiceCollection services, IConfiguration config)
+        {
+            return services.Configure<ApplicationSettings>(config.GetSection(nameof(ApplicationSettings)));
+        }
+    }
+}
