815ae667750aafef2b0f076811c0e56b13be0b93
vladperchi <vladperchi@gmail.com>
2022-05-28 13:43:33 -0500

    feat(DEV): Added Application Layer

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/ApplicationLayerExtensions.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationLayerExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using HureIT.Shared.Core.Interfaces.Services;
+using HureIT.Shared.Core.Settings;
+using HureIT.Shared.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace HureIT.Shared.Infrastructure.Extensions
+{
+    public static class ApplicationLayerExtensions
+    {
+        internal static IServiceCollection AddApplicationLayer(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddTransient<ILoggerService, LoggerService>();
+            services.AddScoped<IJobService, HangfireService>();
+            services.AddTransient<IUploadService, UploadService>();
+            services.AddTransient<IMailService, SmtpMailService>();
+            services.AddTransient<ITemplateMailService, TemplateMailService>();
+            services.AddTransient<IExcelService, ExcelService>();
+            services.Configure<MailSettings>(config.GetSection(nameof(MailSettings)));
+            services.Configure<TemplateMailSettings>(config.GetSection(nameof(TemplateMailSettings)));
+            services.Configure<CorsSettings>(config.GetSection(nameof(CorsSettings)));
+            services.Configure<SwaggerSettings>(config.GetSection(nameof(SwaggerSettings)));
+
+            return services;
+        }
+    }
+}
