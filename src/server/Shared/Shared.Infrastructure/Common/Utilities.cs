1e827d2f50feb73e9e7ca6e623dd2c4dde0fc6ff
vladperchi <vladperchi@gmail.com>
2022-05-28 11:52:26 -0500

    feat(DEV): Added Common Utilities

---
+++ src/server/Shared/Shared.Infrastructure/Common/Utilities.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="Utilities.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using HureIT.Shared.Core.Constants;
+
+namespace HureIT.Shared.Infrastructure.Common
+{
+    public static class Utilities
+    {
+        private static readonly Random Random = new Random();
+
+        public static List<T> GetAllConstantValues<T>(this Type type)
+        {
+            return type
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(T))
+                .Select(x => (T)x.GetRawConstantValue())
+                .ToList();
+        }
+
+        public static List<string> GetNestedStringValues(this Type type)
+        {
+            var values = new List<string>();
+            foreach (var prop in type.GetNestedTypes().SelectMany(c => c.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)))
+            {
+                object propertyValue = prop.GetValue(null);
+                if (propertyValue is not null)
+                {
+                    values.Add(propertyValue.ToString());
+                }
+            }
+
+            return values;
+        }
+
+        public static Task<string> GenerateCode(string letter, int length)
+        {
+            const string chars = StringKeys.AlphaNumeric;
+            return Task.FromResult(new string(Enumerable
+                .Repeat($"{letter}-{chars}-{StringKeys.AppName.ToUpper()}", length)
+                .Select(s => s[Random.Next(s.Length)])
+                .ToArray()).ToLower());
+        }
+    }
+}
