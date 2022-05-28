e17bd62872fc881b70d2871b5641b1153df519bb
vladperchi <vladperchi@gmail.com>
2022-05-28 13:43:18 -0500

    feat(DEV): Added Application Builder

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationBuilderExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.IO;
+using System.Runtime.CompilerServices;
+using HureIT.Shared.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using Serilog.Events;
+using HureIT.Shared.Core.Constants;
+
+[assembly: InternalsVisibleTo(StringKeys.AssemblyName)]
+
+namespace HureIT.Shared.Infrastructure.Extensions
+{
+    internal static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseSharedInfrastructure(this IApplicationBuilder app, IConfiguration config)
+        {
+            app.UseExceptionMiddleware();
+            app.UseSecurityHeaders(config);
+            app.UseRouting();
+
+            string filesDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "Files");
+            if (!Directory.Exists(filesDirectoryPath))
+            {
+                Directory.CreateDirectory(filesDirectoryPath);
+            }
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Files")),
+                RequestPath = new PathString("/Files")
+            });
+            app.UseSerilogRequestLogging(options =>
+            {
+                options.MessageTemplate = "Handled {RequestPath}";
+                options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+                options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+                {
+                    diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+                    diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+                };
+            });
+            app.UseCorsPolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseHangfireDashboard(config);
+            app.UseSwaggerDocumentation(config);
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers().RequireAuthorization();
+            });
+            app.Initialize();
+
+            return app;
+        }
+
+        internal static IApplicationBuilder Initialize(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            foreach (var initializer in serviceScope.ServiceProvider.GetServices<IDbSeeder>())
+            {
+                initializer.Initialize();
+            }
+
+            return app;
+        }
+    }
+}
