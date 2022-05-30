4434b8676ccf0d12edd2d367dbd882debb7817aa
vladperchi <vladperchi@gmail.com>
2022-05-29 21:20:55 -0500

    feat(identity): Construct Model Context

---
+++ src/server/Modules/Identity/Modules.Identity.Infrastructure/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ModelBuilderExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using HureIT.Modules.Identity.Core.Entities;
+using HureIT.Shared.Core.Settings;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HureIT.Modules.Identity.Infrastructure.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyIdentityConfiguration(this ModelBuilder builder, PersistenceSettings persistenceOptions)
+        {
+            if (persistenceOptions.UseMsSql)
+            {
+                foreach (var property in builder.Model.GetEntityTypes()
+                    .SelectMany(t => t.GetProperties())
+                    .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+                {
+                    property.SetColumnType("decimal(23,2)");
+                }
+            }
+
+            builder.Entity<HureUser>(entity =>
+            {
+                entity.ToTable(name: "Users");
+            });
+            builder.Entity<HureRole>(entity =>
+            {
+                entity.ToTable(name: "Roles");
+            });
+            builder.Entity<HureRoleClaim>(entity =>
+            {
+                entity.ToTable(name: "RoleClaims");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.RoleClaims)
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+            });
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("UserTokens");
+            });
+        }
+    }
+}
