d57bd38b6cbf3bee3618f81875f02044bde13fcf
vladperchi <vladperchi@gmail.com>
2022-05-29 21:03:47 -0500

    feat(identity): Added Get Error Messages

---
+++ src/server/Modules/Identity/Modules.Identity.Infrastructure/Extensions/IdentityResultExtensions.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IdentityResultExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Modules.Identity.Infrastructure.Extensions
+{
+    internal static class IdentityResultExtensions
+    {
+        public static List<string> GetErrorMessages(this IdentityResult result, IStringLocalizer localizer) =>
+            result.Errors.Select(x => localizer[x.Description].ToString()).ToList();
+    }
+}
