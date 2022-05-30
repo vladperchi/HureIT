f6fb481c54ff9c826dc6d09c01ed2e4b16d0be27
vladperchi <vladperchi@gmail.com>
2022-05-29 20:37:51 -0500

    style(Core): Updated Const User Image Ghost

--- src/server/Shared/Shared.Core/Constants/StringKeys.cs
+++ src/server/Shared/Shared.Core/Constants/StringKeys.cs
@@ -25,8 +25,7 @@ namespace HureIT.Shared.Core.Constants
         public const string CurrentUserGuest = "Guest";
         public const string ClaimsIssuer = "LOCAL AUTHORITY";
         public const string HiddenValue = "*******";
-        public const string StaffImageGhost = "Files\\Images\\Staff\\ghost.png";
-        public const string EmployeeImageDefault = "Files\\Images\\Employees\\default.png";
+        public const string UserImageGhost = "Files\\Images\\Operator\\ghost.png";
         public const string PatternNumber = " ({0})";
         public const string AlphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     }
