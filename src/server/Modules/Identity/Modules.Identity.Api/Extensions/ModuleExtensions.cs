1128c2bfcd471ab22d4890434bb3b47ad7596b17
vladperchi <vladperchi@gmail.com>
2022-05-29 21:26:09 -0500

    chore(identity): Update Api Project & XML

---
+++ src/server/Modules/Identity/Modules.Identity.Api/Extensions/ModuleExtensions.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ModuleExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using HureIT.Modules.Identity.Core.Extensions;
+using HureIT.Modules.Identity.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HureIT.Modules.Identity.Api.Extensions
+{
+    public static class ModuleExtensions
+    {
+        public static IServiceCollection AddIdentityModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddIdentityCore()
+                .AddIdentityInfrastructure(configuration);
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityModule(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
