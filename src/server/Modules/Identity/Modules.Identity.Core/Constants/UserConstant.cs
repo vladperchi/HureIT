d1e2c7a0c8f86538c070627005b790c2dc2819c0
vladperchi <vladperchi@gmail.com>
2022-05-29 20:42:55 -0500

    feat(identity): Added User Const

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Constants/UserConstant.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UserConstant.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Modules.Identity.Core.Constants
+{
+    public static class UserConstant
+    {
+        public const string AdministratorPassword = "@4dm1nP4$$w0rd#";
+        public const string StaffPassword = "@B4s1cP4$$w0rd#";
+
+        // You can add how many user you want to handle
+    }
+}
