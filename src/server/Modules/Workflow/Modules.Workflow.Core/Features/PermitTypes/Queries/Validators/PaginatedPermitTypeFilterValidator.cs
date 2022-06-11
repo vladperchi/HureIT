b90af6bcf1dd773d3d2184ea763c5f735f9f943e
vladperchi <vladperchi@gmail.com>
2022-06-10 23:39:16 -0500

    feat(DEV): Added PermitType Filter Validator

---
+++ src/server/Modules/Workflow/Modules.Workflow.Core/Features/PermitTypes/Queries/Validators/PaginatedPermitTypeFilterValidator.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PaginatedPermitTypeFilterValidator.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using HureIT.Modules.Workflow.Core.Entities;
+using HureIT.Shared.Core.Features.Queries.Validators;
+using HureIT.Shared.DTO.Workflow.PermitTypes;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Modules.Workflow.Core.Features.PermitTypes.Queries.Validators
+{
+    public class PaginatedPermitTypeFilterValidator : PaginatedFilterValidator<Guid, PermitType, PaginatedPermitTypeFilter>
+    {
+        /// <summary>
+        /// You can override the validation rules here.
+        /// </summary>
+        public PaginatedPermitTypeFilterValidator(IStringLocalizer<PaginatedPermitTypeFilterValidator> localizer)
+            : base(localizer)
+        {
+        }
+    }
+}
