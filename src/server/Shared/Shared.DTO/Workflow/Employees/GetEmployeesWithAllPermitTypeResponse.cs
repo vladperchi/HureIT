78326a04043b57b4cd8232759932ee1927dbac43
vladperchi <vladperchi@gmail.com>
2022-06-10 23:17:24 -0500

    refactor(DTO): Update Employee Response

---
+++ src/server/Shared/Shared.DTO/Workflow/Employees/GetEmployeesWithAllPermitTypeResponse.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GetEmployeesWithAllPermitTypeResponse.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace HureIT.Shared.DTO.Workflow.Employees
+{
+    public class GetEmployeesWithAllPermitTypeResponse
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Admission { get; set; }
+    }
+}
---
+++ src/server/Shared/Shared.DTO/Workflow/Employees/GetEmployeesWithPermitsResponse.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GetEmployeesWithPermitsResponse.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace HureIT.Shared.DTO.Workflow.Employees
+{
+    public record GetEmployeesWithPermitsResponse(Guid Id, string FullName, string PhoneNumber, string Email)
+    {
+        public int TotalPermits { get; set; }
+
+        public IEnumerable<string> PermitList { get; set; }
+
+    }
+}
