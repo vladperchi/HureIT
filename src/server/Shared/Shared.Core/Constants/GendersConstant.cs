717405393d25ef865e504178856cd43e34075489
vladperchi <vladperchi@gmail.com>
2022-06-11 10:22:23 -0500

    feat(DEV): Added Genders Constant

---
+++ src/server/Shared/Shared.Core/Constants/GendersConstant.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GendersConstant.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace HureIT.Shared.Core.Constants
+{
+    public static class GendersConstant
+    {
+        public static class GenderType
+        {
+            public const string Female = "Female";
+            public const string Male = "Male";
+        }
+    }
+}
