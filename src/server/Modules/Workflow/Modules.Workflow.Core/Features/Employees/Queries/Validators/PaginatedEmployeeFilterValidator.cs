f10b25c3441bb232e8b78075da4f292c73b9ee1f
vladperchi <vladperchi@gmail.com>
2022-06-10 23:09:15 -0500

    feat(DEV): Added Employee FilteValidator

---
+++ src/server/Modules/Workflow/Modules.Workflow.Core/Features/Employees/Queries/Validators/PaginatedEmployeeFilterValidator.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PaginatedEmployeeFilterValidator.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using HureIT.Modules.Workflow.Core.Entities;
+using HureIT.Shared.Core.Features.Queries.Validators;
+using HureIT.Shared.DTO.Workflow.Employees;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Modules.Workflow.Core.Features.Employees.Queries.Validators
+{
+    public class PaginatedEmployeeFilterValidator : PaginatedFilterValidator<Guid, Employee, PaginatedEmployeeFilter>
+    {
+        /// <summary>
+        /// You can override the validation rules here.
+        /// </summary>
+        public PaginatedEmployeeFilterValidator(IStringLocalizer<PaginatedEmployeeFilterValidator> localizer)
+            : base(localizer)
+        {
+        }
+    }
+}
