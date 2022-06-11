c11b8123b8f80a3c898c9f1cc816edde018a31a3
vladperchi <vladperchi@gmail.com>
2022-06-11 09:58:57 -0500

    feat(DEV): Added Permit Exceptions

---
+++ src/server/Modules/Workflow/Modules.Workflow.Core/Exceptions/PermitCustomException.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PermitCustomException.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Extensions.Localization;
+using HureIT.Shared.Core.Exceptions;
+
+namespace HureIT.Modules.Workflow.Core.Exceptions
+{
+    public class PermitCustomException : CustomException
+    {
+        public PermitCustomException(IStringLocalizer localizer, List<string> errors)
+            : base(localizer["Permit errors have occurred..."], errors, HttpStatusCode.InternalServerError)
+        {
+        }
+    }
+}
---
+++ src/server/Modules/Workflow/Modules.Workflow.Core/Exceptions/PermitListEmptyException.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PermitListEmptyException.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Net;
+using HureIT.Shared.Core.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace HureIT.Modules.Workflow.Core.Exceptions
+{
+    public class PermitListEmptyException : CustomException
+    {
+        public PermitListEmptyException(IStringLocalizer localizer)
+            : base(localizer["Permit list is empty..."], null, HttpStatusCode.NoContent)
+        {
+        }
+    }
+}
