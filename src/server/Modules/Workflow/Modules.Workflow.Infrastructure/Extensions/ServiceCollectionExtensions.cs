fe0692695a9c4e68272b57e55f846d622d4936eb
vladperchi <vladperchi@gmail.com>
2022-06-11 10:40:44 -0500

    feat(DEV): Added Services Extensions

---
+++ src/server/Modules/Workflow/Modules.Workflow.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="HureIT">
+// Copyright (c) HureIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using HureIT.Modules.Workflow.Core.Abstractions;
+using HureIT.Modules.Workflow.Infrastructure.Persistence;
+using HureIT.Modules.Workflow.Infrastructure.Services;
+using HureIT.Shared.Core.Interfaces.Services;
+using HureIT.Shared.Core.Interfaces.Services.Workflow;
+using HureIT.Shared.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HureIT.Modules.Workflow.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWorkflowInfrastructure(this IServiceCollection services)
+        {
+            services.AddDatabaseContext<WorkflowDbContext>();
+            services.AddScoped<IWorkflowDbContext>(provider => provider.GetService<WorkflowDbContext>());
+            services.AddTransient<IDbSeeder, WorkflowDbSeeder>();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient<IEmployeeService, EmployeeService>();
+            services.AddTransient<IPermitTypeService, PermitTypeService>();
+            services.AddTransient<IPermitService, PermitService>();
+            return services;
+        }
+
+        public static IServiceCollection AddWorkflowValidation(this IServiceCollection services)
+        {
+            services.AddControllers().AddWorkflowValidation();
+            return services;
+        }
+    }
+}
